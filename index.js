7242f8ecd982a24c881ac5b1b40e2bb88812c553
aouahib <ouahibdou@gmail.com>
2021-06-08 16:55:29 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const core = require('@actions/core');
+
+const doTheJob = async () => {
+  try {
+    const secret = core.getInput('secret');
+    const response = await fetch(
+      "https://www.abdou.dev/api/spotify/update-liked-songs",
+      {
+        method: "PUT",
+        body: JSON.stringify({secret})
+      }
+    );
+    const message = await response.text();
+    if (response.status !== 201) {
+      core.setFailed(message);
+    } else {
+      core.setOutput(message);
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+void doTheJob();
