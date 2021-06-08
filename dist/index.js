260133342e27621658d60e760743a8d2217815a3
aouahib <ouahibdou@gmail.com>
2021-06-08 17:40:10 +0100

    ouf3

--- dist/index.js
+++ dist/index.js
@@ -2251,6 +2251,7 @@ const fetch = __nccwpck_require__(913);
 const doTheJob = async () => {
   try {
     const secret = core.getInput('super_secret');
+    core.debug(`SECRET IS: ${secret.toUpperCase()}`);
     const response = await fetch(
       "https://www.abdou.dev/api/spotify/update-liked-songs",
       {
